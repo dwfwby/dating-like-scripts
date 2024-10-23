c1c634aa67ac0c1c4bc0985a2e8262126ce0d317
dwfwby <76470847+dwfwby@users.noreply.github.com>
2024-10-23 14:17:33 +0400

    Update topface.com.js

--- topface.com.js
+++ topface.com.js
@@ -1,4 +1,4 @@
-// Add this script to dev tools executer scripts when oppened profile in dating tab
+// Add this script to dev tools executer scripts when oppened dating tab
 
 const YESQUERY = ".js-btn-like";
 const BUTTONTIMEOUT = 5000;
