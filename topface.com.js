2cd886aaeaafa625e1856abf8fd2abd3a7c5f14b
dwfwby <76470847+dwfwby@users.noreply.github.com>
2024-10-23 12:51:18 +0400

    Create topface.com.js

---
+++ topface.com.js
@@ -0,0 +1,31 @@
+// Add this script to dev tools executer scripts when oppened profile in dating tab
+
+const YESQUERY = ".js-btn-like";
+const BUTTONTIMEOUT = 5000;
+const WAITCLICK = 2000;
+
+function sleep(delay){
+  return new Promise( r => r(setTimeout(delay)));
+}
+
+function getButton(query){
+    return new Promise((resolve, reject) => {
+        const startTime = new Date().getTime();
+        setInterval(() => {
+            const time = new Date().getTime()
+            let nextButton = document.querySelector(query);
+            if(nextButton)
+                resolve(nextButton)
+            else if(time - startTime >= BUTTONTIMEOUT)
+                resolve();
+        }, 100)
+    })
+}
+
+let yesButton;
+while(yesButton = await getButton(YESQUERY)){
+
+    await sleep(WAITCLICK);    
+    yesButton.click();
+
+}
