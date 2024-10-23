42a9f0bc6167455997a369f210b94f5203211c71
dwfwby <76470847+dwfwby@users.noreply.github.com>
2024-10-23 12:52:09 +0400

    Create loveplanet.ru.js

---
+++ loveplanet.ru.js
@@ -0,0 +1,37 @@
+// Add this script to dev tools executer scripts when oppened profile in dating tab
+
+const NOQUERY = "#likes-btn-no";
+const YESQUERY = "#likes-btn-yes";
+const BUTTONTIMEOUT = 5000;
+const WAITCLICK = 2000;
+
+function sleep(delay){
+  return new Promise( r => r(setTimeout(delay)));
+}
+
+function getButton(query){
+    return new Promise((resolve, reject) => {
+        const startTime = new Date().getTime();
+        setInterval(() => {
+            const time = new Date().getTime()
+            let nextButton = document.querySelector(query);
+            if(nextButton)
+                resolve(nextButton)
+            else if(time - startTime >= BUTTONTIMEOUT)
+                resolve();
+        }, 100)
+    })
+}
+
+let noButton;
+while(noButton = await getButton(NOQUERY)){
+    const yesButton = await getButton(YESQUERY);
+    await sleep(WAITCLICK);
+    if(!yesButton){
+        noButton.click();
+        continue;
+    }
+    
+    yesButton.click();
+
+}
