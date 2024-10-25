d118f4221736d88ed17af4f7dcc06d0ec86467c6
dwfwby <76470847+dwfwby@users.noreply.github.com>
2024-10-25 21:41:51 +0400

    Update and rename love.ru.js to default/love.ru.js

---
+++ default/love.ru.js
@@ -0,0 +1,9 @@
+// Add this script to dev tools executer scripts when oppened dating tab
+// Exec on site love.ru/znakomstva/
+
+const config = {
+    yesquery: "#sympathy_btn_send",
+    skipquery: "#sympathy_btn_reject",
+    elementtimeout: 5000,
+    waitlike: 500
+}
