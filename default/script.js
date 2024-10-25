72b66d8590b791d4c386a82b551e66a80c5d9f11
dwfwby <76470847+dwfwby@users.noreply.github.com>
2024-10-25 21:20:22 +0400

    Update script.js

--- default/script.js
+++ default/script.js
@@ -1,5 +1,5 @@
 async function sdf34dsf(){
-    async function checker(callback, delay, timeout){
+    async function setIntervalMax(callback, delay, timeout){    
         let resolver;
         const promise = new Promise( r => resolver = r);
         const startTime = new Date().getTime();
@@ -11,7 +11,6 @@ async function sdf34dsf(){
                 resolver(result);
             else if(time - startTime >= timeout)
                 resolver();
-            
         }, delay);
         
         await promise;
@@ -20,28 +19,37 @@ async function sdf34dsf(){
     }
     
     function getElement(query, timeout){
-        return checker(function(){ return document.querySelector(query)}, 100, timeout);
-    }
+        return setIntervalMax(function(){ return document.querySelector(query)}, 100, timeout);
+}
     
-    function waitDifference(callback, old, timeout){
-        return checker(async function(){ 
+    function onChange(callback, old, timeout){
+        return setIntervalMax(async function(){ 
             const res = await callback();
             
             return res != old && res;
         }, 100, timeout);
     }
     
-    const sleep_ = delay => new Promise( r => setTimeout(r, delay))
+    const sleep = delay => new Promise( r => setTimeout(r, delay))
     
-    const getCompared = async () => (await getElement(config.comparedquery, config.comparedtimeout)).outerHTML;
+    const getCompared = async () => (await getElement(config.comparedquery, config.comparedtimeout))?.outerHTML;
     
     let compared, newCompared, yesButton;
     
-    const hasNewCompared = () => waitDifference(async () => await getCompared(), compared, config.comparedtimeout);
+    const hasNewCompared = () => onChange(async () => await getCompared(), compared, config.comparedtimeout);
     
-    while((newCompared = await hasNewCompared()) && (yesButton = await getElement(config.yesquery, config.elementtimeout))){
+    while(newCompared = await hasNewCompared()){
+        yesButton = await getElement(config.yesquery, config.elementtimeout);
+
+        if(!yesButton && config.skipquery)
+            yesButton = await getElement(config.skipquery, config.elementtimeout);
+        
+        if(!yesButton)
+            break;
+        
         compared = newCompared;
-        await sleep_(config.waitlike);
+        
+        await sleep(config.waitlike);
         yesButton.click();
     }
 }
